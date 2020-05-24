bdd520314fb1c38659a00b0b1739bf70aefdbd83
Jake Young <need2edit@gmail.com>
2020-05-24 16:22:41 -0400

    Adds shadow utilities

---
+++ Sources/JYHelpers/Layouts/SketchShadows.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jake Young on 4/19/20.
+//
+
+import UIKit
+
+// https://stackoverflow.com/questions/34269399/how-to-control-shadow-spread-and-blur/48489506
+public extension CALayer {
+  func applySketchShadow(
+    color: UIColor = .black,
+    alpha: Float = 0.5,
+    x: CGFloat = 0,
+    y: CGFloat = 2,
+    blur: CGFloat = 4,
+    spread: CGFloat = 0)
+  {
+    shadowColor = color.cgColor
+    shadowOpacity = alpha
+    shadowOffset = CGSize(width: x, height: y)
+    shadowRadius = blur / 2.0
+    if spread == 0 {
+      shadowPath = nil
+    } else {
+      let dx = -spread
+      let rect = bounds.insetBy(dx: dx, dy: dx)
+      shadowPath = UIBezierPath(rect: rect).cgPath
+    }
+  }
+}
